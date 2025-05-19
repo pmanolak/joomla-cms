8ab0eaace61e3f3a6dc3f63e1c87c96eddfccff5
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2025-05-20 00:53:10 +0200

    Revert to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc1';
+    public const EXTRA_VERSION = 'rc2';
 
     /**
      * Development status.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '23:00';
+    public const RELTIME = '23:01';
 
     /**
      * Release timezone.
